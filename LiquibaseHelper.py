e1f3611cc6f7d144c98ca49ccd73c8828aa44c75
Chris Seals <cseals@ball.com>
2022-05-04 13:54:19 -0500

    update functionality - check headers and ids are correct

--- LiquibaseHelper.py
+++ LiquibaseHelper.py
@@ -5,8 +5,13 @@ import fileinput
 import sys
 from numpy import real
 import pandas as pd
+import os
 
 version = 'v1.5.0'
+
+# TODO make parser ensure the ID matches the filename
+# TODO return a list of files that have no --liquibase formatted sql at the top
+# TODO identify missing changesets
 
 def changeset_parser(line: str) -> dict:
     """ parses a given changeset into a dictionary format"""
@@ -31,6 +36,31 @@ def changeset_parser(line: str) -> dict:
         raise Exception('NO ID FOUND FOR CHANGESET: '+ line)
     return changeset_dict
 
+def prepend_line(file_name, line):
+    """ Insert given string as a new line at the beginning of a file """
+    # define name of temporary dummy file
+    dummy_file = file_name + '.bak'
+    # open original file in read mode and dummy file in write mode
+    with open(file_name, 'r') as read_obj, open(dummy_file, 'w') as write_obj:
+        # Write given line to the dummy file
+        write_obj.write(line + '\n')
+        # Read lines from original file one by one and append them to the dummy file
+        for line in read_obj:
+            write_obj.write(line)
+    # remove original file
+    os.remove(file_name)
+    # Rename dummy file as the original file
+    os.rename(dummy_file, file_name)
+
+def check_headers(file):
+        file1 = open(file, 'r')
+        lines = file1.readlines()
+        # usage: 
+        print(lines[0])
+        if '--liquibase formatted sql' not in lines[0]:
+            # add --liquibase formatted sql to header
+            prepend_line(file, '--liquibase formatted sql')
+
 def increment_changeset(file):
     n = 1
     for line in fileinput.input(file, inplace=1):
@@ -40,6 +70,8 @@ def increment_changeset(file):
                 line = line.replace('.sql-x', f'.sql-{n}')
                 line = line.replace('.sql ', f'.sql-{n} ')
                 n+=1
+            if file.split('/')[-1] not in line:
+                line = re.sub('(?<=:)\S+\.sql', file.split('/')[-1], line)
         sys.stdout.write(line)
 
 def export_changesets_to_csv(changeset_dict: dict, filename: str):
